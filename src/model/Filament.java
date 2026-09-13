fcb1f8e787e1a736f793233a7e0cc626919981e1
Mirasik <aituaraferist7b@gmail.com>
2026-09-13 14:08:46 +0500

    Added class Filament and basic structure for it

---
+++ src/model/Filament.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Filament {
+    private String Name;
+    private String manufacturerName;
+    private double nozzleTemperature;
+    private double bedTemperature;
+    private double pricePerKg;
+    private double weightGram;
+    private String color;
+    private boolean isFlexible;
+
+
+}

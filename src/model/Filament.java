2f07dbd0f0b87a7194ce93008c85aba87cc3fd2a
Mirasik <aituaraferist7b@gmail.com>
2026-09-13 14:27:00 +0500

    Added builder + some exceptions for it

--- src/model/Filament.java
+++ src/model/Filament.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Filament {
-    private String Name;
+    private String name;
     private String manufacturerName;
     private double nozzleTemperature;
     private double bedTemperature;
@@ -10,5 +10,45 @@ public class Filament {
     private String color;
     private boolean isFlexible;
 
+    public Filament(FilamentBuilder builder){
+        this.name = builder.name;
+        this.nozzleTemperature = builder.nozzleTemperature;
+        this.bedTemperature = builder.bedTemperature;
+        this.pricePerKg = builder.pricePerKg;
+        this.weightGram = builder.weightGram;
+        this.color = builder.color;
+        this.isFlexible = builder.isFlexible;
+    }
 
+    public String getName(){
+        return name;
+    }
+
+    public String getManufacturerName(){
+        return manufacturerName;
+    }
+
+    public double getNozzleTemperature(){
+        return nozzleTemperature;
+    }
+
+    public double getBedTemperature(){
+        return bedTemperature;
+    }
+
+    public double getPricePerKg() {
+        return pricePerKg;
+    }
+
+    public double getWeightGram() {
+        return weightGram;
+    }
+
+    private String getColor(){
+        return color;
+    }
+
+    private boolean isFlexible(){
+        return isFlexible;
+    }
 }
---
+++ src/model/FilamentBuilder.java
@@ -0,0 +1,78 @@
+package model;
+
+public class FilamentBuilder {
+    String name;
+    String manufacturerName;
+    double nozzleTemperature;
+    double bedTemperature;
+    double pricePerKg;
+    double weightGram;
+    String color="White";
+    boolean isFlexible=false;
+
+
+    public FilamentBuilder setName(String name){
+        this.name = name;
+        return this;
+    }
+
+
+    public FilamentBuilder setManufacturerName(String manufacturerName){
+        this.manufacturerName = manufacturerName;
+        return this;
+    }
+
+
+    public FilamentBuilder setNozzleTemperature(double nozzleTemperature){
+        this.nozzleTemperature = nozzleTemperature;
+        return this;
+    }
+
+
+    public FilamentBuilder setBedTemperature(double bedTemperature){
+        this.bedTemperature = bedTemperature;
+        return this;
+    }
+
+
+    public FilamentBuilder setPricePerKg(double pricePerKg){
+        this.pricePerKg = pricePerKg;
+        return this;
+    }
+
+    public FilamentBuilder setWeightGram(double weightGram){
+        this.weightGram = weightGram;
+        return this;
+    }
+
+    public FilamentBuilder setColor(String color){
+        this.color = color;
+        return this;
+    }
+
+    public FilamentBuilder setFlexible(boolean isFlexible) {
+        this.isFlexible = isFlexible;
+        return this;
+    }
+
+    public Filament build() {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalStateException("Filament name cannot be empty");
+        }
+        if (nozzleTemperature < 150 || nozzleTemperature > 450) {
+            throw new IllegalArgumentException("Invalid nozzle temperature: " + nozzleTemperature);
+        }
+        if (manufacturerName == null || manufacturerName.trim().isEmpty()) {
+            throw new IllegalStateException("Filament Manufacturer name cannot be empty");
+        }
+        if (bedTemperature < 20 || bedTemperature > 150) {
+            throw new IllegalArgumentException("Invalid nozzle temperature: " + bedTemperature);
+        }
+        return new Filament(this);
+    }
+
+
+
+
+
+}
